55cc98a9e5f84740ccec639cc0c189a094861d2b
d1zaynerS <sergdrotyank@gmail.com>
2025-09-30 18:28:34 +0300

    Добавил домашку

---
+++ dz/dz-1/ConsoleApplication3/ConsoleApplication3.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include <iomanip>
+
+int main()
+{
+    double pi;
+    std::cout << "Enter a double: ";
+    std::cin >> pi;
+
+    int value = static_cast<int>(pi);
+
+    std::cout << "Double: " << pi << std::endl;
+    std::cout << "Integer: " << value << std::endl;
+}
---
+++ dz/dz-2/ConsoleApplication4/ConsoleApplication4.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+
+void modify(char* str) {
+    str[0] = 'H';
+}
+
+int main() {
+    // Массив char — память изменяемая
+    char text[] = "hello";
+
+    modify(text);
+
+    std::cout << text << std::endl; // Выведет: Hello
+}
