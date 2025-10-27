26d444fff886c7351b54a357645c2c0e2800b394
d1zaynerS <sergdrotyank@gmail.com>
2025-10-27 18:24:51 +0200

    Добавил домашку

---
+++ dz/dz-1/ConsoleApp15/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Завдання 1: квадрат");
+
+            Console.Write("Введіть довжину сторони квадрата: ");
+            int size = int.Parse(Console.ReadLine());
+
+            Console.Write("Введіть символ для квадрата: ");
+            char symbol = Console.ReadLine()[0];
+
+            DrawSquare(size, symbol);
+        }
+
+        public static void DrawSquare(int side, char symbol)
+        {
+            for (int i = 0; i < side; i++)
+            {
+                for (int j = 0; j < side; j++)
+                {
+                    Console.Write(symbol);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ dz/dz-2/ConsoleApp16/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введіть число: ");
+            int number = int.Parse(Console.ReadLine());
+
+            bool isPal = IsPalindrome(number);
+
+            if (isPal)
+                Console.WriteLine("Число " + number + " є паліндромом.");
+            else
+                Console.WriteLine("Число " + number + " НЕ є паліндромом.");
+        }
+        public static bool IsPalindrome(int num)
+        {
+            int original = num;
+            int reverse = 0;
+
+            while (num > 0)
+            {
+                int digit = num % 10;
+                reverse = reverse * 10 + digit;
+                num /= 10;
+            }
+
+            return original == reverse;
+        }
+    }
+}
