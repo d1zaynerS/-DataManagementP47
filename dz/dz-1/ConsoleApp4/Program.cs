3861402da2eb20e78b11c2fd77ef4d3ebdf409ce
d1zaynerS <sergdrotyank@gmail.com>
2025-10-27 18:01:05 +0200

    Добавил домашку

---
+++ dz/dz-1/ConsoleApp4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введіть число від 1 до 100: ");
+            int n = int.Parse(Console.ReadLine());
+            if (n < 1 || n > 100)
+                Console.WriteLine("Помилка! Число має бути в діапазоні 1–100.");
+            else if (n % 3 == 0 && n % 5 == 0)
+                Console.WriteLine("Fizz Buzz");
+            else if (n % 3 == 0)
+                Console.WriteLine("Fizz");
+            else if (n % 5 == 0)
+                Console.WriteLine("Buzz");
+            else
+                Console.WriteLine(n);
+        }
+    }
+}
---
+++ dz/dz-7/ConsoleApp10/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введіть перше число: "); 
+            int a = Convert.ToInt32(Console.ReadLine()); 
+            Console.Write("Введіть друге число: "); 
+            int b = Convert.ToInt32(Console.ReadLine()); 
+            if (a > b) 
+                (a, b) = (b, a); 
+            Console.WriteLine("Парні числа у діапазоні:"); 
+            for (int i = a; i <= b; i++) 
+                if (i % 2 == 0) 
+                    Console.Write(i + " "); 
+            Console.WriteLine();
+        }
+    }
+}
