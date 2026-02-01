a5e96ec99bdd2add1e4ace137c1f23c0a159123d
d1zaynerS <sergdrotyank@gmail.com>
2026-02-01 10:51:00 +0200

    Добавил домашку

---
+++ HomeWork(1)/Program.cs
@@ -0,0 +1,9 @@
+﻿using DataManagementP47.Ado;
+
+Console.WriteLine("Запуск процесса создания таблиц...");
+
+var ado = new Ado();
+await ado.Run();
+
+Console.WriteLine("\nГотово! Нажми любую клавишу, чтобы выйти.");
+Console.ReadKey();
