6568fc5a5b33978868979fa54f35393d10411ee7
d1zaynerS <sergdrotyank@gmail.com>
2026-02-01 17:31:50 +0200

    Добавил домашку

--- HomeWork(1)/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿using DataManagementP47.Ado;
-
-Console.WriteLine("Запуск процесса создания таблиц...");
-
-var ado = new Ado();
-await ado.Run();
-
-Console.WriteLine("\nГотово! Нажми любую клавишу, чтобы выйти.");
-Console.ReadKey();
---
+++ dz/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace SystemProgrammingP47
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("System Programming");
+            new ProcessDemo().Run();
+        }
+    }
+}
